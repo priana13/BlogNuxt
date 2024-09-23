ffe4b2effda3de08391cb17a17e37b1bbc445002
priana13 <herian.sap@gmail.com>
2024-09-24 06:08:45 +0700

    Instalasi Nuxt Js

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template lang="">
+  <div>
+   <h2> Home </h2>
+
+   <p>Belajar NuxtJs </p>
+    
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+  
+</style>
---
+++ pages/products/product-detail.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>Detail Produk</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
