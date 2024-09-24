964cb3dae333dde3b02087465ad7d6334050b772
priana13 <herian.sap@gmail.com>
2024-09-24 20:29:36 +0700

    route paramenter

---
+++ pages/products/[id_produk].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+     <p>Detail produk untuk id : {{ id_produk }}</p>
+    </div>
+</template>
+
+<script setup>
+   const { id_produk } = useRoute().params
+</script>
+
+<style lang="scss" scoped>
+
+</style>
